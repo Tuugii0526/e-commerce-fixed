158b60efc0fd84a3c30a9a5c96ac1d3ffabde8a8
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-10-19 09:29:05 +0800

    if vercel is working

--- front-end/app/admin/page.jsx
+++ front-end/app/admin/page.jsx
@@ -1,3 +1,18 @@
+"use client"
+import { useState} from "react"
+import { requestPassword } from "@/lib/actions"
 export default function Page(){
-    return <div> Iam admin</div>
+    const [password,setPassword]=useState('')
+
+    return (
+        <form action={requestPassword}>
+            <input type="text" placeholder={password} name="password" className="ring-1" onChange={(e)=>{
+                setPassword(e.target.value)
+            }}/>
+            <button className="btn" type="submit" onClick={()=>{
+                setPassword('')
+            }}>Submit</button>
+            <p>Hello world</p>
+        </form>
+    )
 }
--- front-end/lib/actions.js
+++ front-end/lib/actions.js
@@ -9,3 +9,25 @@ export const fetchCustomers=async ()=>{
     const customers=await sql `SELECT * FROM  customers`
     return customers
 }
+export async function requestPassword(formData){
+    const reg=/^[^_][^\s]{3,}/
+    const password=formData.get('password')
+    console.log('password is:',password)
+    if(!password)
+    {
+        return null
+    }
+    if(!reg.test(password))
+    {
+        return null
+    }
+    try {
+        await sql`
+    INSERT INTO test(password)
+    VALUES(${password})
+    `
+    return {status:"success"}
+    } catch (error) {
+        return null
+    }
+    }
